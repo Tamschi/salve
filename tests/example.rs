99b589bc7551511d6ade712c6bea79da744d1355
Tamme Schichler <tamme@schichler.dev>
2022-06-30 22:06:59 +0200

    Added example test, also to README

---
+++ tests/example.rs
@@ -0,0 +1,38 @@
+use catch::CatchExt;
+use proc_macro2::{Ident, Span, TokenStream};
+use quote::quote_spanned;
+use salve::ParseOrForgeExt;
+use syn::{parse::ParseStream, Error, Token};
+
+pub fn parse_triple(input: ParseStream, errors: &mut Vec<Error>) -> (Ident, Token![,], Ident) {
+	(
+		// Parsing will continue here regardless of errors, yielding dummy values.
+		// Errors are collected for later use.
+		input.parse_or_forge().catch_item(errors),
+		input.parse_or_forge().catch_item(errors),
+		input.parse_or_forge().catch_item(errors),
+	)
+}
+
+pub fn macro_transform(input: ParseStream) -> TokenStream {
+	let mut errors = vec![];
+	let (first, _, second) = parse_triple(input, &mut errors);
+	if !input.is_empty() {
+		errors.push(Error::new_spanned(
+			input.parse::<TokenStream>().expect("infallible"),
+			"Unexpected tokens.",
+		))
+	}
+	let errors = errors.into_iter().map(Error::into_compile_error);
+	quote_spanned! {Span::mixed_site()=>
+		#(#errors)*
+
+		// Even if `second` is unavailable, `first` may be present, reducing errors elsewhere.
+		#[automatically_derived]
+		struct #first;
+
+		// Even if `first` is unavailable, `seconds` may be present, reducing errors elsewhere.
+		#[automatically_derived]
+		struct #second;
+	}
+}
